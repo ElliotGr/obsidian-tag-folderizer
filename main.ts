dd7f2f955988faa671e986aa2af7e2aa72279937
ElliotGr <elliotgravestock@hotmail.co.uk>
2022-07-17 22:11:06 +0100

    Split into multiple functions

--- main.ts
+++ main.ts
@@ -1,18 +1,33 @@
-import { getAllTags, Plugin} from "obsidian";
+import { CachedMetadata, getAllTags, Plugin } from "obsidian";
 
 export default class tagFolderizer extends Plugin {
-  async onload() {
-    this.registerEvent(this.app.metadataCache.on('changed', (changedFile, fileText, fileCache) => {
-      console.log('File updated')
+	async onload() {
+		this.registerEvent(this.app.metadataCache.on('changed', (file, data, cache) => handleChanges(cache)));
+	}
+}
 
-	  const allTags = getAllTags(fileCache)
-	  if (allTags == null || allTags.length === 0) {
-		console.log('No tags found')
-		return;
-	  } 
+function handleChanges(fileCache: CachedMetadata) {
+	const firstTag = getFirstTag(fileCache);
+	const tagParts = splitTag(firstTag);
+	console.log(tagParts)
+}
 
-	  const firstTag = allTags[0]
-	  const tagParts = firstTag.substring(1).split('/')
-    }));
-  }
+function getFirstTag(fileCache: CachedMetadata): string | null {
+	console.log('File updated');
+
+	const allTags = getAllTags(fileCache);
+	if (allTags == null || allTags.length === 0) {
+		console.log('No tags found');
+		return null;
+	} else {
+		return allTags[0]
+	}
+}
+
+function splitTag(tag: string | null): string[] | null {
+	if (tag) {
+		return tag.substring(1).split('/');
+	} else {
+		return null;
+	}
 }
