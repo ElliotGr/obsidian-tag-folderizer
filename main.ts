52a1d93da8574f8b7977cc32c389288da29e4abe
ElliotGr <elliotgravestock@hotmail.co.uk>
2022-07-17 19:09:29 +0100

    Add logic to find first tag

--- main.ts
+++ main.ts
@@ -1,10 +1,24 @@
-import { Plugin } from "obsidian";
+import { MetadataCache, parseFrontMatterTags, Plugin } from "obsidian";
 
 export default class tagFolderizer extends Plugin {
   async onload() {
     this.registerEvent(this.app.metadataCache.on('changed', (changedFile, fileText, fileCache) => {
       console.log('File updated')
-	  console.log(fileCache)
+
+	  let firstTag
+	  const frontmatterTags = parseFrontMatterTags(fileCache.frontmatter)
+	  if (frontmatterTags) {
+		firstTag = frontmatterTags[0]
+	  }
+	  else if (fileCache.tags) {
+		firstTag = fileCache.tags[0].tag
+	  }
+	  else {
+		console.log('No Tags Found')
+		return
+	  }
+
+	  console.log(firstTag)
     }));
   }
 }
